4f6aaf76cbe087d8c3bc75fb3d2a913651f0f19d
Alexander Bespalko <alexander.bespalko@sigma.software>
2024-03-05 16:54:22 +0200

    Load sdk from stage.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,9 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <script src="https://stage-admin-editor.bound360.com/editor-sdk/bound-360.min.js" async></script>
+          <script src="https://stage-admin-editor.bound360.com/editor-sdk/bound-360.min.js"
+                  async
+          ></script>
         </body>
       </Html>
     );
